064c556903d3aacddb531b62e07eeb1272622eb2
tn <tn@ex.com>
2021-08-13 16:01:51 +0800

    first commit

---
+++ src/main/java/com/test/es/test/ESTest_Client.java
@@ -0,0 +1,18 @@
+package com.test.es.test;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+
+public class ESTest_Client {
+    public static void main(String[] args) throws Exception {
+
+        // 创建ES客户端
+        RestHighLevelClient esClient = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("192.168.1.175", 9200, "http"))
+        );
+
+        // 关闭ES客户端
+        esClient.close();
+    }
+}
